dbe4c0fa9add123952856b2dc90872f81a8a3d12
Shush06 <143515172+Shush95@users.noreply.github.com>
2024-02-20 04:12:59 +0100

    Add files via upload

---
+++ Kinobilletter.js
@@ -0,0 +1,43 @@
+let tickets = [];{
+
+}
+function buyTicket (){
+    const movie = document.getElementById('movie').value.trim();
+
+    const quantity =parseInt(document.getElementById('quantity').value);
+
+    const name = document.getElementById('name').value.trim();
+
+    const surname = document.getElementById('surname').value.trim();
+
+    const phone = document.getElementById('phone').value.trim();
+
+    const email = document.getElementById('email').value.trim();
+   if(name === ""){
+       alert("vennligst fyll ut feltene korrekt");
+       return;
+   }
+
+ const ticket = {
+     movie: movie,
+     quantity: quantity,
+     name: name,
+     surname:surname,
+     phone: phone,
+     email: email
+ };
+ tickets.push(ticket);
+ displayTickets();
+ document.getElementById(movie).value= " ";
+    document.getElementById(quantity).value= " ";
+    document.getElementById(name).value= " ";
+    document.getElementById(surname).value= " ";
+    document.getElementById(phone).value= " ";
+    document.getElementById(email).value= " ";
+}
+function displayTickets() {
+    const  ticketList = document.getElementById('ticketList');
+    ticketList.innerHTML = "";
+    tickets.forEach(ticket => {
+        const listItem = document.createElement('li');
+        for (const emailElement of listItem.textContent = 'Velg film: ${ticket
