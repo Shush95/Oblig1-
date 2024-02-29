afe2cc7656d4958e1426882b7ccfd585f780de0f
Shush06 <143515172+Shush95@users.noreply.github.com>
2024-02-29 14:00:06 +0100

    Update Kinobilletter.js

--- Kinobilletter.js
+++ Kinobilletter.js
@@ -13,8 +13,28 @@ function buyTicket (){
     const phone = document.getElementById('phone').value.trim();
 
     const email = document.getElementById('email').value.trim();
-   if(!movie === ""|| name === ""|| surname === ""|| phone === "" || email === ""|| isNaN(quantity) <= 0){
-       alert("vennligst fyll ut feltene korrekt");
+   if (!movie===" ") {
+       alert(" Vennligst velg en av valgene ");
+       return;
+   }
+   if (name === ""){
+       alert ("Vennligst fyll ut dette feltet");
+       return;
+    }
+    if(surname === ""){
+        alert ("Vennligst fyll ut feltet for å gå videre");
+        return;
+    }
+    if (phone === ""){
+        alert("Vennligst oppgi et gyldig nummer");
+        return;
+    }
+    if (email === ""){
+        alert ("Feltet må fyllest ut riktig for å gå videre");
+        return;
+    }
+    if(isNaN(quantity) <= 0){
+       alert("Du må oppgi større antall enn 0");
        return;
    }
 
